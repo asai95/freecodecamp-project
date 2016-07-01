a246960776d6ddf3488078a3ccd6d00b95d8ead4
Viktor Zhemchuzhnikov <viktr5909@mail.ru>
2016-07-01 22:03:57 +0000

    finished

---
+++ timestamp.js
@@ -0,0 +1,15 @@
+var express = require("express")
+var app = express()
+
+app.get("/:param", (req, resp) => {
+    if (Number(req.params.param)) {
+        var date = new Date(parseInt(req.params.param)*1000)
+        console.log(date)
+    } else {var date = new Date(req.params.param)}
+    if (date != "Invalid Date") {
+        resp.json({unix: Date.parse(date)/1000, natural: date.toDateString()})
+    } else {resp.json({unix: null, natural: null})}
+    resp.end()
+})
+
+app.listen(8080)
